614913da9505482bbd29f5c6c88fd18d42f651a0
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-16 14:34:51 +0700

    :tractor: change packages

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,9 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, SafeAreaView, View, Text, Pressable } from "react-native";
 import Modal from "react-native-modal";
-import * as RNLocalize from 'react-native-localize';
-import i18n from 'i18n-js';
-import memoize from 'lodash.memoize';
 
 import AppButton from "../components/button";
 import SizedBox from "../components/sized-box";

2a0302f8b34a196b0dae4f817caaaacb9bad2468
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2022-11-16 16:13:50 +0700

    :pencil: update strings

--- src/translations/en.tsx
+++ src/translations/en.tsx
@@ -1,4 +1,8 @@
 export const en = {
     "welcome": "Welcome",
-    "app_name": "RN App Language"
+    "app_name": "RN App Language",
+    "information": "This app is available in english and indonesia language, change language settings below.",
+    "settings": "Settings",
+    "change_language": "Change Language",
+    "selected_language": "English"
 }
